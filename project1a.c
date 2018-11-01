58c505939bf5fac7c69c086a4d92d36c1e296963
Bio <bioshimv@gmail.com>
2018-10-31 22:23:26 -0700

    project1a.c

--- project1a.c
+++ project1a.c
@@ -13,39 +13,61 @@ void addGuess(int start[][9],int board[][9], int row, int col, int value){
     }
 }
 
-int checkPuzzle(int board[][9]){
-    int i,j,k,l,m,n;
-	
-	// i = row
-	// j = index of first value
-	// k = index of 2nd value
-    for(i=0;i<9;i++){
-        for(j=0;j<9;j++){
-            for(k=j+1;k<9;k++){
-            if (board[i][j] != 0 && board[i][k] != 0 && board[j][i] != 0 && board[k][i] != 0 && (board[i][j]==board[i][k]||board[j][i]==board[k][i])) {
-                return 0;
+int checkPuzzle(int board[][9]) {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			// i: will refer to row (aka x value)
+			// j: will refer to col (aka y value)
+			// int i = row;
+			// int j = col;
+			for (int k = 0; k < 9; k++) {
+				// Check per row
+				if (board[i][k] == board[i][j] && k != j) {
+					return 0;
+				}
+						
+				// Check per col
+				if (board[k][j] == board[i][j] && k != i) {
+					return 0;
+				}
 			}
-			}
-        }
-    }
 
-    for(i=0;i<9;i+=3){
-        for(j=0;j<9;j+=3){
-            for(k=i;k<3;k++){
-                for(l=j;l<3;l++){
-                    for(m=k;m<3;m++){
-                        for(n=l;n<3;n++){
-                            if(k!=m||n!=l&&board[k][l]==board[m][n] && board[k][l] != 0 && board[m][n] != 0){
-                                return 0;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return 1;
+
+			int startRow = 0;
+			int startCol = 0;
+
+			if(i < 3) {
+				startRow = 0;
+			} else if (i < 6) {
+				startRow = 3;
+			} else {
+				startRow = 6;
+			}
+
+			if (j < 3) {
+				startCol = 0;
+			} else if (j < 6) {
+				startRow = 3;
+			} else {
+				startCol = 6;
+			}
+
+
+			for (int m = startRow; m < startRow + 3; m++) {
+				for (int n = startCol; n < startCol + 3; n++) {
+					if(board[m][n] == board[i][j]) {
+						return 0;
+					}
+				}
+			}
+		}
+	}
+
+		return 1;
+
 }
+	
+
 // Idk if you want me to do the functions, so getAllowedValues can take a piece from checkpuzzle, and I think checkPuzzle can be changed a bit
 
 int checkValue(int board[][9], int row, int col, int value) {
@@ -61,12 +83,13 @@ int checkValue(int board[][9], int row, int col, int value) {
 				
 		// Check per col
 		if (board[k][j] == value && k != i) {
-			return 0;
+	return 0;
 		}
 	}
 
-	int startRow;
-	int startCol;
+
+	int startRow = 0;
+	int startCol = 0;
 
 	if(row < 3) {
 		startRow = 0;
@@ -78,11 +101,12 @@ int checkValue(int board[][9], int row, int col, int value) {
 
 	if (col < 3) {
 		startCol = 0;
-	} else if (row < 6) {
+	} else if (col < 6) {
 		startRow = 3;
 	} else {
 		startCol = 6;
 	}
+
 
 	for (int m = startRow; m < startRow + 3; m++) {
 		for (int n = startCol; n < startCol + 3; n++) {
@@ -119,24 +143,15 @@ void reset(int defaultBoard[9][9], int board[9][9]){
 }
 
 void getAllowedValues(int board[][9], int row, int col, int arr[]){
-    int i;
-
-    for(i=1;i<=9;i++){
+	//printf("Did we start loop\n");
+    for(int i = 1; i < 10; i++){
         if(checkValue(board, row, col, i)){
 		//if(checkPuzzle(board)) {
-			printf("We workin\n");
             arr[i-1]=1;
         }else{
             arr[i-1]=0;
         }
     }
-
-	printf("\n");
-	for (int i = 0; i < 9; i++) {
-		printf("%d", arr[i]);
-	}
-	printf("\n");
-	printf("Finished hi\n");
 }
 
 // Sets the given square to the given value as an initial value that cannot be changed by the puzzle solver
@@ -161,13 +176,26 @@ void createSudokuPuzzle(int board[9][9], int defaultBoard[9][9], int start[9][9]
 	}
 
 	for (int i = 0; i < 30; i++) {
-		// What I'm doing here is, I'll generate a number between 0 and 100. How this applys to a board is:
-		// Every position is between 0 and 9 for columns and 0 and 9 for rows. Thsi means, a coordinate is always between
-		// 0, 0 and 9,9. Through this, the tens place can eb found and the ones place can be found to develop a coordinate point.
-		//srand(time(NULL));
-		hereIsGood[i] = rand() % 100;
+		int unique = 1;
+		do {
+			// What I'm doing here is, I'll generate a number between 0 and 100. How this applys to a board is:
+			// Every position is between 0 and 9 for columns and 0 and 9 for rows. Thsi means, a coordinate is always between
+			// 0, 0 and 9,9. Through this, the tens place can eb found and the ones place can be found to develop a coordinate point.
+			hereIsGood[i] = rand() % 100;
+			unique = 1;
+
+			for (int j = 0; j < i; j++) {
+				if(hereIsGood[i] == hereIsGood[j]) {
+					unique = 0;
+				}
+			}
+		} while (unique == 0);
+
+		
+
 		int x = hereIsGood[i] / 10;
 		int y = hereIsGood[i] % 10;
+
 
 		start[x][y] = 0;
 			
@@ -176,14 +204,8 @@ void createSudokuPuzzle(int board[9][9], int defaultBoard[9][9], int start[9][9]
 			
 		while(!gotItIn) {
 			int value = (rand() % 9) + 1; 
-			printf("Hi\n");
 			getAllowedValues(board, x, y, valid);
-			printf("Hi2\n");
 
-			
-			printf("Value %d is %d\n", value, valid[value]);
-		
-				
 			if (valid[value-1]) { //Uncomment this for real program
 				addInitial(board, x, y, value); //Uncomment this for real program
 				addInitial(defaultBoard, x, y, value);
@@ -191,7 +213,6 @@ void createSudokuPuzzle(int board[9][9], int defaultBoard[9][9], int start[9][9]
 			}
 				 
 		}
-		printf("Loop!\n");
 	}
 }
 
@@ -284,6 +305,7 @@ void main() {
 				printf("This value will not work. Feel free to change it.\n");
 			}
 
+			/*
 			
 			if(isFull(board) && checkPuzzle(board)) {
 				printf("\nYou win!\n");
@@ -301,6 +323,7 @@ void main() {
 					gameStillGoing = 0;
 				}
 			}
+			*/
 
 		}
 
